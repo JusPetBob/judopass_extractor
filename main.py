249392b13c31baab88c7ab78e3b06a31e6514cc2
JusPetBob <JusPetBob@gmail.com>
2026-04-12 17:23:58 +0200

    added camerapreview

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from kivy.lang import Builder
 from kivy.properties import StringProperty, ObjectProperty
 from kivy.utils import platform
+from kivy.clock import Clock
 
 from kivymd.app import MDApp
 from kivymd.uix.navigationbar import MDNavigationBar, MDNavigationItem
 from kivymd.uix.screen import MDScreen
+
+from camera4kivy.preview import Preview
+from cv2 import QRCodeDetector,cvtColor, COLOR_RGBA2BGR, COLOR_BGR2GRAY
+from numpy import frombuffer,uint8
+from pyzbar.pyzbar import decode
+from yarl import URL
+import jwt
 
 # base navigation class 
 class BaseMDNavigationItem(MDNavigationItem):
@@ -19,6 +27,28 @@ class BaseMDNavigationItem(MDNavigationItem):
 # base qrcode scanner screen class
 class ScannerScreen(MDScreen):
     image_size = StringProperty()
+
+
+class CPreview(Preview):
+    detector = QRCodeDetector()
+    
+    def __init__(self, **kwargs):
+        super().__init__(aspect_ratio = '16:9', **kwargs)
+    
+    def analyze_pixels_callback(self, pixels, image_size, image_pos, image_scale, mirror):
+        w, h = image_size
+        
+        frame = frombuffer(pixels, dtype=uint8).reshape(h, w, 4)
+        
+        frame = cvtColor(frame, COLOR_RGBA2BGR)
+        
+        gray = cvtColor(frame, COLOR_BGR2GRAY)
+        
+        for qr in decode(gray):
+            data = qr.data.decode()
+            if data.startswith("https://"):
+                token = URL(data).query("s")
+                print(jwt.decode(token, options={"verify_signature":False}))
 
 # base list screen class
 class ListScreen(MDScreen):
@@ -40,6 +70,11 @@ class App(MDApp):
 
     def build(self):
         return Builder.load_file("app.kv")
+    
+    def on_start(self):
+        screen = self.root.ids.screen_manager.get_screen("Scanner")
+        self.camera = screen.ids.camera
+        self.camera.connect_camera(enable_analyze_pixels=True)
 
 
 if __name__ == "__main__":
