a9a68126d40ca1e13e759b5e712b43303d34ece4
JusPetBob <JusPetBob@gmail.com>
2026-04-12 15:46:19 +0200

    initial app layout

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-from kivymd.app import MDApp
 from kivy.lang import Builder
+from kivy.properties import StringProperty, ObjectProperty
 
-class MainApp():
-    pass
+from kivymd.app import MDApp
+from kivymd.uix.navigationbar import MDNavigationBar, MDNavigationItem
+from kivymd.uix.screen import MDScreen
 
+# base navigation class 
+class BaseMDNavigationItem(MDNavigationItem):
+    badge = ObjectProperty(None)
+    icon = StringProperty()
+    text = StringProperty()
+
+    def set_badge(self, value):
+        if self.badge:
+            self.badge.text = str(value)
+
+# base qrcode scanner screen class
+class ScannerScreen(MDScreen):
+    image_size = StringProperty()
+
+# base list screen class
+class ListScreen(MDScreen):
+    image_size = StringProperty()
+
+
+#standard app
 class App(MDApp):
+    def on_switch_tabs(
+            self,
+            bar: MDNavigationBar,
+            item: MDNavigationItem,
+            item_icon: str,
+            item_text: str,
+        ):
+        self.root.ids.screen_manager.current = item_text
+        
+        #self.root.ids.list_item.set_badge(5)
+
     def build(self):
         return Builder.load_file("app.kv")
 
